77a9bf08f49d12175c9cde20b50e2bdd581437c3
levantiendat <levan040669@gmail.com>
2023-05-02 16:26:53 +0700

    commit layout

--- app/src/main/java/com/example/dk88/AdminProfileActivity.java
+++ app/src/main/java/com/example/dk88/AdminProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.dk88;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -107,7 +108,8 @@ public class AdminProfileActivity extends AppCompatActivity {
                     }
                 }
                 else{
-
+                    Intent intent=new Intent(AdminProfileActivity.this,UserRequestAdapter.class);
+                    startActivity(intent);
                 }
             }
         });
