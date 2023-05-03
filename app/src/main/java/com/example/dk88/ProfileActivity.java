36e00dd27d8743705b9c636f6c2851d0d1edb55b
levantiendat <levan040669@gmail.com>
2023-05-03 09:41:27 +0700

    commit change 32

--- app/src/main/java/com/example/dk88/ProfileActivity.java
+++ app/src/main/java/com/example/dk88/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.dk88;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,36 +45,11 @@ public class ProfileActivity extends AppCompatActivity {
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Map<String,Object> changeInfo=new HashMap<>();
-                changeInfo.put("userName",student.getUserName());
-                changeInfo.put("name",edtName.getText().toString());
-                changeInfo.put("phoneNumber",edtPhone.getText().toString());
-                changeInfo.put("roleCode",Integer.toString(student.getRoleCode()));
+                if((edtOld.getText().toString().compareTo("")==0&&edtName.getText().toString().compareTo(student.getName())==0)
+                        &&(edtPhone.getText().toString().compareTo(student.getPhoneNumber())==0)){
 
-                Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().changeProfile(changeInfo);
-                call.enqueue(new Callback<ResponseObject>() {
-                    @Override
-                    public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
-                        if (!response.isSuccessful()) {
-                            Toast.makeText(ProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
-                            return;
-                        }
-                        ResponseObject tmp = response.body();
-                        if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
-                            Toast.makeText(ProfileActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
-                            return;
-                        }
-                        Map<String, Object> data = (Map<String, Object>) tmp.getData();
-                        String userRole = response.headers().get("UserRole");
-                        Toast.makeText(ProfileActivity.this, "Change Data successfully ", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseObject> call, Throwable t) {
-                        Toast.makeText(ProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
-                    }
-                });
-                if(edtOld.getText().toString()!=""){
+                }
+                if(edtOld.getText().toString().compareTo("")!=0){
                     if(edtOld.getText().toString().compareTo(edtNew.getText().toString())==0){
                         Toast.makeText(ProfileActivity.this,"The new password is duplicated than old password",Toast.LENGTH_LONG).show();
                     }
@@ -107,6 +83,45 @@ public class ProfileActivity extends AppCompatActivity {
                         });
                     }
                 }
+                if(!(edtName.getText().toString().compareTo(student.getName())==0
+                        &&(edtPhone.getText().toString().compareTo(student.getPhoneNumber())==0))) {
+                    Map<String, Object> changeInfo = new HashMap<>();
+                    changeInfo.put("userName", student.getUserName());
+                    changeInfo.put("name", edtName.getText().toString());
+                    changeInfo.put("phoneNumber", edtPhone.getText().toString());
+                    changeInfo.put("roleCode", Integer.toString(student.getRoleCode()));
+
+                    Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().changeProfile(changeInfo);
+                    call.enqueue(new Callback<ResponseObject>() {
+                        @Override
+                        public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
+                            if (!response.isSuccessful()) {
+                                Toast.makeText(ProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                            ResponseObject tmp = response.body();
+                            if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
+                                Toast.makeText(ProfileActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                            Map<String, Object> data = (Map<String, Object>) tmp.getData();
+                            String userRole = response.headers().get("UserRole");
+                            Toast.makeText(ProfileActivity.this, "Change Data successfully ", Toast.LENGTH_LONG).show();
+                            Intent intent=new Intent(ProfileActivity.this,UserRequest.class);
+                            intent.putExtra("token",token);
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponseObject> call, Throwable t) {
+                            Toast.makeText(ProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(ProfileActivity.this, "Nothing change information", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
 
