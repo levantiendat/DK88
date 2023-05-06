14dd4906ffcb02a5d68199f701fa7da6f03a1156
levantiendat <levan040669@gmail.com>
2023-05-07 00:57:06 +0700

    commit new layout

--- app/src/main/java/com/example/dk88/StudentIdentity.java
+++ app/src/main/java/com/example/dk88/StudentIdentity.java
@@ -1,24 +1,31 @@
 package com.example.dk88;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import androidx.core.app.ActivityCompat;
 
 public class StudentIdentity extends AppCompatActivity {
-    ImageView imgFront,imgBack;
+    private static final int MY_REQUEST_CODE = 1000;
+    ImageButton imgFront,imgBack;
+    ImageView image;
+    private static final String TAG=StudentIdentity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +35,72 @@ public class StudentIdentity extends AppCompatActivity {
         String token=getIntent().getStringExtra("token");
         Student student=(Student) getIntent().getSerializableExtra("student");
 
-        imgFront=(ImageView) findViewById(R.id.imgFront);
-        imgBack=(ImageView) findViewById(R.id.imgBack);
+        imgFront= findViewById(R.id.imgFront);
+        imgBack=(ImageButton) findViewById(R.id.imgBack);
+        image=(ImageView) findViewById(R.id.picture);
 
         imgFront.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                startActivityForResult(intent,0);
+                onClickRequestPermission();
             }
         });
         imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                startActivityForResult(intent,0);
+                onClickRequestPermission();
             }
         });
+    }
+    private ActivityResultLauncher<Intent> mActivityResultLauncher=registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Log.e(TAG,"onActivityResult");
+                    if(result.getResultCode()== Activity.RESULT_OK){
+                        Intent data=result.getData();
+                        if(data==null){
+                            return;
+                        }
+                        Uri uri=data.getData();
+                        try{
+                            Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                            image.setImageBitmap(bitmap);
+                        } catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
+    private void onClickRequestPermission() {
+
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+            openGallery();
+        }
+        else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==MY_REQUEST_CODE){
+            if(grantResults.length>0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED){
+                openGallery();
+            }
+        }
+    }
+
+    private void openGallery() {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        mActivityResultLauncher.launch(Intent.createChooser(intent,"Select Picture"));
     }
 }
