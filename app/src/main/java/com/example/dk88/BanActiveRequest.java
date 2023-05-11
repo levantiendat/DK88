fee60442aef8ff03559878d628a4e823cc3091ae
levantiendat <levan040669@gmail.com>
2023-05-11 15:05:16 +0700

    commit new view

---
+++ app/src/main/java/com/example/dk88/BanActiveRequest.java
@@ -0,0 +1,13 @@
+package com.example.dk88;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BanActiveRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
