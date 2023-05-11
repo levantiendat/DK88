cf51706a8cb37fa787785b8d30ab37387b1ceacd
levantiendat <levan040669@gmail.com>
2023-05-11 15:08:38 +0700

    commit new view

--- app/src/main/java/com/example/dk88/BanRequestDetail.java
+++ app/src/main/java/com/example/dk88/BanRequestDetail.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BanActiveRequest extends AppCompatActivity {
+public class BanRequestDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
