b86d5f109cbb089585e851751dcb8206f9da3667
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-06-01 20:10:23 +0700

    Update Admin Dashboard

---
+++ app/src/main/java/com/example/dk88/AdminDashboard.java
@@ -0,0 +1,65 @@
+package com.example.dk88;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AdminDashboard extends AppCompatActivity {
+    CardView cvUsers, cvRequest, cvProfile, cvLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_dashboard_layout);
+        String token=getIntent().getStringExtra("token");
+        Admin admin=(Admin) getIntent().getSerializableExtra("admin");
+
+        cvUsers=(CardView) findViewById(R.id.userManagement);
+        cvRequest=(CardView) findViewById(R.id.requestManagement);
+        cvProfile=(CardView) findViewById(R.id.adminProfile);
+        cvLogout=(CardView) findViewById(R.id.logout);
+
+
+        cvLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminDashboard.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cvProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminDashboard.this, AdminProfileActivity.class);
+                intent.putExtra("token",token);
+                intent.putExtra("admin",admin);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cvRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminDashboard.this, UserRequest.class);
+                intent.putExtra("token",token);
+                intent.putExtra("admin",admin);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cvUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+
+    }
+}
