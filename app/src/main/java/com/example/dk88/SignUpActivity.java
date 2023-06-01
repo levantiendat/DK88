1aba905c6439db227a97fdc390493dcc01204b8c
levantiendat <levan040669@gmail.com>
2023-06-01 14:45:27 +0700

    finish SignUpActivity

--- app/src/main/java/com/example/dk88/SignUpActivity.java
+++ app/src/main/java/com/example/dk88/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.example.dk88;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +23,8 @@ import retrofit2.Response;
 public class SignUpActivity extends AppCompatActivity {
     Button btnOk;
     TextView back;
-    EditText edtFullName, edtPhone, edtID,edtUser,edtPass;
+    boolean passwordVisible = false;
+    EditText edtFullName, edtPhone, edtID,edtUser,edtPass,edtFacebook,edtPass1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +37,13 @@ public class SignUpActivity extends AppCompatActivity {
         edtUser=(EditText) findViewById(R.id.Username);
         edtPass=(EditText) findViewById(R.id.Password);
         back=(TextView) findViewById(R.id.back);
+        edtFacebook= (EditText) findViewById(R.id.facebookLink);
+        edtPass1=(EditText) findViewById(R.id.ConfirmPassword);
 
+        final Drawable eyeDrawable = getResources().getDrawable(R.drawable.eye);
+        eyeDrawable.setBounds(0, 0, eyeDrawable.getIntrinsicWidth(), eyeDrawable.getIntrinsicHeight());
+        edtPass.setCompoundDrawables(null, null, eyeDrawable, null);
+        edtPass1.setCompoundDrawables(null, null, eyeDrawable, null);
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,29 +53,37 @@ public class SignUpActivity extends AppCompatActivity {
                 registerInfo.put("studentID",edtID.getText().toString());
                 registerInfo.put("name",edtFullName.getText().toString());
                 registerInfo.put("phoneNumber",edtPhone.getText().toString());
-                Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().signup(registerInfo);
-                call.enqueue(new Callback<ResponseObject>() {
-                    @Override
-                    public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
-                        if (!response.isSuccessful()) {
-                            Toast.makeText(SignUpActivity.this, "Error", Toast.LENGTH_LONG).show();
-                            return;
-                        }
-                        ResponseObject tmp = response.body();
-                        if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
-                            Toast.makeText(SignUpActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
-                            return;
-                        }
-                        Map<String, Object> data = (Map<String, Object>) tmp.getData();
-                        String userRole = response.headers().get("UserRole");
-                        Toast.makeText(SignUpActivity.this, "Register success as " + data.get("name"), Toast.LENGTH_LONG).show();
-                    }
+                registerInfo.put("facebook",edtFacebook.getText().toString());
+                if(edtPass.getText().toString().compareTo(edtPass1.getText().toString())==0) {
 
-                    @Override
-                    public void onFailure(Call<ResponseObject> call, Throwable t) {
-                        Toast.makeText(SignUpActivity.this, "Error", Toast.LENGTH_LONG).show();
-                    }
-                });
+
+                    Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().signup(registerInfo);
+                    call.enqueue(new Callback<ResponseObject>() {
+                        @Override
+                        public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
+                            if (!response.isSuccessful()) {
+                                Toast.makeText(SignUpActivity.this, "Error", Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                            ResponseObject tmp = response.body();
+                            if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
+                                Toast.makeText(SignUpActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
+                                return;
+                            }
+                            Map<String, Object> data = (Map<String, Object>) tmp.getData();
+                            String userRole = response.headers().get("UserRole");
+                            Toast.makeText(SignUpActivity.this, "Register success as " + data.get("name"), Toast.LENGTH_LONG).show();
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponseObject> call, Throwable t) {
+                            Toast.makeText(SignUpActivity.this, "Error", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(SignUpActivity.this, "Your password is not compare to confirm password", Toast.LENGTH_LONG).show();
+                }
             }
         });
         back.setOnClickListener(new View.OnClickListener() {
@@ -73,6 +91,44 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent=new Intent(SignUpActivity.this,SignInActivity.class);
                 startActivity(intent);
+            }
+        });
+        edtPass.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (edtPass.getRight() - edtPass.getCompoundDrawables()[2].getBounds().width())) {
+                        // Xử lý sự kiện bấm vào biểu tượng con mắt ở đây
+                        passwordVisible = !passwordVisible;
+
+                        if (passwordVisible) {
+                            edtPass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        } else {
+                            edtPass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        edtPass1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (edtPass1.getRight() - edtPass1.getCompoundDrawables()[2].getBounds().width())) {
+                        // Xử lý sự kiện bấm vào biểu tượng con mắt ở đây
+                        passwordVisible = !passwordVisible;
+
+                        if (passwordVisible) {
+                            edtPass1.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        } else {
+                            edtPass1.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                        }
+                        return true;
+                    }
+                }
+                return false;
             }
         });
     }
