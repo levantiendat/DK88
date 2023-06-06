99b3ff0626b6c144d9bdde1d6bc35bfc1043b5e9
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-06-06 16:21:28 +0700

    Clean Code AdminActiveRequestDetailActivity.java

--- app/src/main/java/com/example/dk88/AdminActiveRequestDetailActivity.java
+++ app/src/main/java/com/example/dk88/AdminActiveRequestDetailActivity.java
@@ -26,34 +26,36 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class AdminActiveRequestDetailActivity extends AppCompatActivity {
-    TextView edtStudentId;
-    ImageView imgFront, imgBack;
-    Button btnAccept, btnDecline;
-    String token;
-    Request request;
+    private TextView edtStudentId;
+    private ImageView imgFront, imgBack;
+    private Button btnAccept, btnDecline;
+    private String token;
+    private Request request;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_active_request_layout);
-        token=getIntent().getStringExtra("token");
-        request= (Request) getIntent().getSerializableExtra("request");
 
-        edtStudentId = (TextView) findViewById(R.id.studentId);
-        imgFront = (ImageView) findViewById(R.id.imgFront);
-        imgBack = (ImageView) findViewById(R.id.imgBack);
-        btnAccept = (Button) findViewById(R.id.accept);
-        btnDecline = (Button) findViewById(R.id.decline);
+        // Nhận dữ liệu từ Intent
+        getDataFromIntent();
 
+        // Khởi tạo các view
+        initView();
 
+        // Lấy dữ liệu từ server
         getData();
+
+        // Xử lý sự kiện khi nhấn nút "Accept"
         btnAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 isAccept(true);
             }
         });
+
+        // Xử lý sự kiện khi nhấn nút "Decline"
         btnDecline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,93 +64,33 @@ public class AdminActiveRequestDetailActivity extends AppCompatActivity {
         });
     }
 
-    private void getData(){
+    // Nhận dữ liệu từ Intent
+    private void getDataFromIntent() {
+        token = getIntent().getStringExtra("token");
+        request = (Request) getIntent().getSerializableExtra("request");
+    }
+
+    // Khởi tạo các view
+    private void initView() {
+        edtStudentId = findViewById(R.id.studentId);
+        imgFront = findViewById(R.id.imgFront);
+        imgBack = findViewById(R.id.imgBack);
+        btnAccept = findViewById(R.id.accept);
+        btnDecline = findViewById(R.id.decline);
+    }
+
+    // Lấy dữ liệu từ server
+    private void getData() {
         Map<String, Object> header = new HashMap<>();
         header.put("token", token);
-        Map<String,Object> detail = new HashMap<>();
-        detail.put("requestID",request.getRequestID());
-        detail.put("targetID",request.getTargetID());
-        detail.put("requestCode",request.getRequestCode());
-        Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().readDetailRequest(header,detail);
 
-        call.enqueue(new Callback<ResponseObject>() {
-            @Override
-            public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
-                if (!response.isSuccessful()){
-                    Toast.makeText(AdminActiveRequestDetailActivity.this,"Error",Toast.LENGTH_LONG).show();
-                    return;
-                }
-                ResponseObject tmp = response.body();
-                token = response.headers().get("token");
+        Map<String, Object> detail = new HashMap<>();
+        detail.put("requestID", request.getRequestID());
+        detail.put("targetID", request.getTargetID());
+        detail.put("requestCode", request.getRequestCode());
 
-                if (tmp.getRespCode()!=ResponseObject.RESPONSE_OK){
-                    Toast.makeText(AdminActiveRequestDetailActivity.this,tmp.getMessage(),Toast.LENGTH_LONG).show();
-                    return;
+        Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().readDetailRequest(header, detail);
 
-                }
-
-                Map<String , Object> data = (Map<String, Object>) tmp.getData();
-                String requestId = data.get("requestID").toString();
-                String targetId = data.get("targetID").toString();
-                edtStudentId.setText(edtStudentId.getText()+targetId);
-
-                String urlFront = data.get("imageFront").toString();
-                String urlBack = data.get("imageBack").toString();
-                loadImage(urlFront, imgFront);
-                loadImage(urlBack, imgBack);
-
-
-            }
-
-            @Override
-            public void onFailure(Call<ResponseObject> call, Throwable t) {
-                Toast.makeText(AdminActiveRequestDetailActivity.this,"Error",Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-    private  void loadImage(String url, ImageView imgTemp){
-        Map<String,Object> header = new HashMap<>();
-        header.put("token",token);
-        Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().readImage(header,url);
-
-        call.enqueue(new Callback<ResponseObject>() {
-            @Override
-            public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
-                if (!response.isSuccessful()){
-                    Toast.makeText(AdminActiveRequestDetailActivity.this,"Error",Toast.LENGTH_LONG).show();
-                    return;
-                }
-                token = response.headers().get("token");
-
-                ResponseObject tmp = response.body();
-                if (tmp.getRespCode()!=ResponseObject.RESPONSE_OK){
-                    Toast.makeText(AdminActiveRequestDetailActivity.this, tmp.getMessage(),Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                String base64 = tmp.getData().toString();
-
-                byte[] decodedString = Base64.decode(base64,Base64.DEFAULT);
-                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString,0,decodedString.length);
-                imgTemp.setImageBitmap(decodedByte);
-            }
-
-            @Override
-            public void onFailure(Call<ResponseObject> call, Throwable t) {
-                Toast.makeText(AdminActiveRequestDetailActivity.this,"Error",Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-    private void isAccept(Boolean accept){
-        Map<String,Object> header=new HashMap<>();
-        header.put("token",token);
-        Map<String,Object> handle =new HashMap<>();
-        handle.put("requestID",request.getRequestID());
-        handle.put("targetID",request.getTargetID());
-        handle.put("requestCode",request.getRequestCode());
-        handle.put("isAccepted",accept);
-
-        Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().handleRequest(header,handle);
         call.enqueue(new Callback<ResponseObject>() {
             @Override
             public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
@@ -156,21 +98,105 @@ public class AdminActiveRequestDetailActivity extends AppCompatActivity {
                     Toast.makeText(AdminActiveRequestDetailActivity.this, "Error", Toast.LENGTH_LONG).show();
                     return;
                 }
-                token=response.headers().get("token");
+
                 ResponseObject tmp = response.body();
-                if (tmp.getRespCode()!=ResponseObject.RESPONSE_OK)
-                {
+                token = response.headers().get("token");
+
+                if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
                     Toast.makeText(AdminActiveRequestDetailActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
                     return;
                 }
-                if(accept){
-                    Toast.makeText(AdminActiveRequestDetailActivity.this,"ACTIVE SUCCESSFULLY",Toast.LENGTH_LONG).show();
+
+                Map<String, Object> data = (Map<String, Object>) tmp.getData();
+                String requestId = data.get("requestID").toString();
+                String targetId = data.get("targetID").toString();
+                edtStudentId.setText(edtStudentId.getText() + targetId);
+
+                String urlFront = data.get("imageFront").toString();
+                String urlBack = data.get("imageBack").toString();
+                loadImage(urlFront, imgFront);
+                loadImage(urlBack, imgBack);
+            }
+
+            @Override
+            public void onFailure(Call<ResponseObject> call, Throwable t) {
+                Toast.makeText(AdminActiveRequestDetailActivity.this, "Error", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    // Tải ảnh từ server
+    private void loadImage(String url, ImageView imgTemp) {
+        Map<String, Object> header = new HashMap<>();
+        header.put("token", token);
+
+        Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().readImage(header, url);
+
+        call.enqueue(new Callback<ResponseObject>() {
+            @Override
+            public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(AdminActiveRequestDetailActivity.this, "Error", Toast.LENGTH_LONG).show();
+                    return;
                 }
-                else{
-                    Toast.makeText(AdminActiveRequestDetailActivity.this,"DECLINED SUCCESSFULLY",Toast.LENGTH_LONG).show();
+
+                token = response.headers().get("token");
+                ResponseObject tmp = response.body();
+
+                if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
+                    Toast.makeText(AdminActiveRequestDetailActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
+                    return;
                 }
-                Intent intent=new Intent(AdminActiveRequestDetailActivity.this, AdminRequestActivity.class);
-                intent.putExtra("token",token);
+
+                String base64 = tmp.getData().toString();
+                byte[] decodedString = Base64.decode(base64, Base64.DEFAULT);
+                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                imgTemp.setImageBitmap(decodedByte);
+            }
+
+            @Override
+            public void onFailure(Call<ResponseObject> call, Throwable t) {
+                Toast.makeText(AdminActiveRequestDetailActivity.this, "Error", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    // Xác nhận chấp nhận hoặc từ chối
+    private void isAccept(Boolean accept) {
+        Map<String, Object> header = new HashMap<>();
+        header.put("token", token);
+
+        Map<String, Object> handle = new HashMap<>();
+        handle.put("requestID", request.getRequestID());
+        handle.put("targetID", request.getTargetID());
+        handle.put("requestCode", request.getRequestCode());
+        handle.put("isAccepted", accept);
+
+        Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().handleRequest(header, handle);
+        call.enqueue(new Callback<ResponseObject>() {
+            @Override
+            public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(AdminActiveRequestDetailActivity.this, "Error", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                token = response.headers().get("token");
+                ResponseObject tmp = response.body();
+
+                if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
+                    Toast.makeText(AdminActiveRequestDetailActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (accept) {
+                    Toast.makeText(AdminActiveRequestDetailActivity.this, "ACTIVE SUCCESSFULLY", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(AdminActiveRequestDetailActivity.this, "DECLINED SUCCESSFULLY", Toast.LENGTH_LONG).show();
+                }
+
+                Intent intent = new Intent(AdminActiveRequestDetailActivity.this, AdminRequestActivity.class);
+                intent.putExtra("token", token);
                 startActivity(intent);
             }
 
