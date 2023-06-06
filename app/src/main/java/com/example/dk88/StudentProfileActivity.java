42fa44a66af12e90d7f664b6649e757a7283441e
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-06-06 20:28:53 +0700

    Clean Code StudentProfileActivity.java

--- app/src/main/java/com/example/dk88/StudentProfileActivity.java
+++ app/src/main/java/com/example/dk88/StudentProfileActivity.java
@@ -29,13 +29,13 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class StudentProfileActivity extends AppCompatActivity {
-    EditText edtOld,edtNew,edtName,edtPhone,edtFacebook;
-    Button btnOK, btnBack;
-    TextView txtGetAdmin;
-    String token,studentID;
-    Student student;
-    TextView getAdmin;
-    String userName;
+    private EditText edtOld, edtNew, edtName, edtPhone, edtFacebook;
+    private Button btnOK, btnBack;
+    private TextView txtGetAdmin;
+    private String token, studentID;
+    private Student student;
+    private TextView getAdmin;
+    private String userName;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -43,140 +43,70 @@ public class StudentProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_profile_layout);
 
-        token=getIntent().getStringExtra("token");
-        studentID=getIntent().getStringExtra("studentID");
-        userName=getIntent().getStringExtra("userName");
-//        ivBack=(ImageView) findViewById(R.id.back);
-        edtOld=(EditText) findViewById(R.id.Password);
-        edtNew=(EditText) findViewById(R.id.Password1);
-        edtName=(EditText) findViewById(R.id.fullname);
-        edtPhone=(EditText) findViewById(R.id.phone);
-        btnOK=(Button) findViewById(R.id.ok);
-        btnBack=(Button) findViewById(R.id.back);
-        txtGetAdmin=(TextView) findViewById(R.id.getAdmin1);
-        edtFacebook=(EditText) findViewById(R.id.facebookLink);
-        getAdmin=(TextView) findViewById(R.id.getAdmin1);
+        // Nhận dữ liệu từ Intent
+        getDataFromIntent();
 
-        LoadDataFromServer(studentID);
+        // Khởi tạo các view
+        initView();
 
-
+        // Hiển thị thông tin của admin
         getAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showAdminInformationPopup();
             }
         });
+
+        // Trở về màn hình Dashboard của sinh viên
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(StudentProfileActivity.this, StudentDashboardActivity.class);
-                intent.putExtra("studentID",studentID);
-                intent.putExtra("token",token);
-                intent.putExtra("userName",userName);
-                startActivity(intent);
+                navigateToStudentDashboard();
             }
         });
 
-
-
+        // Xử lý sự kiện khi người dùng click nút OK
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (edtOld.getText().toString().compareTo("") != 0) {
-                    if (edtOld.getText().toString().compareTo(edtNew.getText().toString()) == 0) {
-                        Toast.makeText(StudentProfileActivity.this, "The new password is duplicated than old password", Toast.LENGTH_LONG).show();
-                    } else {
-                            Map<String, Object> headers = new HashMap<>();
-                            headers.put("token", token);
-                            Map<String, Object> passInfo = new HashMap<>();
-                            passInfo.put("userName", userName);
-                            passInfo.put("oldHashPass", edtOld.getText().toString().trim());
-                            passInfo.put("newHashPass", edtNew.getText().toString().trim());
+                // Thay đổi mật khẩu
+                changePassword();
 
-                            Call<ResponseObject> call1 = ApiUserRequester.getJsonPlaceHolderApi().changePass(headers, passInfo);
-                            call1.enqueue(new Callback<ResponseObject>() {
-                                @Override
-                                public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
-                                    if (!response.isSuccessful()) {
-                                        Toast.makeText(StudentProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
-                                        return;
-                                    }
-                                    ResponseObject tmp = response.body();
-                                    if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
-                                        Toast.makeText(StudentProfileActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
-                                        return;
-                                    }
-
-                                    Toast.makeText(StudentProfileActivity.this, "Change Password successfully ", Toast.LENGTH_LONG).show();
-
-                                }
-
-                                @Override
-                                public void onFailure(Call<ResponseObject> call, Throwable t) {
-                                    Toast.makeText(StudentProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
-                                }
-                            });
-                        }
-                    }
-                    if (!(edtName.getText().toString().compareTo(student.getName()) == 0
-                            && (edtPhone.getText().toString().compareTo(student.getPhoneNumber()) == 0))) {
-                        Map<String, Object> headers = new HashMap<>();
-                        headers.put("token", token);
-                        Map<String, Object> changeInfo = new HashMap<>();
-                        changeInfo.put("userName", userName);
-                        Toast.makeText(StudentProfileActivity.this,edtName.getText().toString()+"--"+edtPhone.getText().toString()+"--"+edtFacebook.getText().toString(),Toast.LENGTH_LONG).show();
-                        changeInfo.put("name", edtName.getText().toString());
-                        changeInfo.put("phoneNumber", edtPhone.getText().toString());
-                        changeInfo.put("facebook", edtFacebook.getText().toString());
-                        changeInfo.put("roleCode", student.getRoleCode());
-
-                        Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().changeProfile(headers, changeInfo);
-                        call.enqueue(new Callback<ResponseObject>() {
-                            @Override
-                            public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
-                                if (!response.isSuccessful()) {
-                                    Toast.makeText(StudentProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
-                                    return;
-                                }
-                                ResponseObject tmp = response.body();
-                                if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
-                                    Toast.makeText(StudentProfileActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
-                                    return;
-                                }
-                                Map<String, Object> data = (Map<String, Object>) tmp.getData();
-
-                                Toast.makeText(StudentProfileActivity.this, "Change Data successfully ", Toast.LENGTH_LONG).show();
-
-                                Intent intent = new Intent(StudentProfileActivity.this, StudentDashboardActivity.class);
-                                intent.putExtra("studentID", studentID);
-                                intent.putExtra("token", token);
-                                startActivity(intent);
-                            }
-
-                            @Override
-                            public void onFailure(Call<ResponseObject> call, Throwable t) {
-                                Toast.makeText(StudentProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
-                            }
-                        });
-                    } else {
-                        Toast.makeText(StudentProfileActivity.this, "Nothing change information", Toast.LENGTH_LONG).show();
-                    }
-                }
-
-
+                // Thay đổi thông tin sinh viên
+                changeStudentInfo();
+            }
         });
-
     }
+
     @Override
     public void onBackPressed() {
-        Intent intent = new Intent(StudentProfileActivity.this, StudentDashboardActivity.class);
-        intent.putExtra("token", token);
-        intent.putExtra("studentID", studentID);
-        intent.putExtra("userName",userName);
-        // Bắt đầu Activity tiếp theo với hiệu ứng chuyển động
-        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
-
+        navigateToStudentDashboard();
     }
+
+    // Nhận dữ liệu từ Intent
+    private void getDataFromIntent() {
+        token = getIntent().getStringExtra("token");
+        studentID = getIntent().getStringExtra("studentID");
+        userName = getIntent().getStringExtra("userName");
+    }
+
+    // Khởi tạo các view
+    private void initView() {
+        edtOld = findViewById(R.id.Password);
+        edtNew = findViewById(R.id.Password1);
+        edtName = findViewById(R.id.fullname);
+        edtPhone = findViewById(R.id.phone);
+        btnOK = findViewById(R.id.ok);
+        btnBack = findViewById(R.id.back);
+        txtGetAdmin = findViewById(R.id.getAdmin1);
+        edtFacebook = findViewById(R.id.facebookLink);
+        getAdmin = findViewById(R.id.getAdmin1);
+
+        // Tải dữ liệu sinh viên từ server
+        loadDataFromServer(studentID);
+    }
+
+    // Hiển thị popup thông tin admin
     private void showAdminInformationPopup() {
         Context context = StudentProfileActivity.this;
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -200,11 +130,102 @@ public class StudentProfileActivity extends AppCompatActivity {
         // Hiển thị dialog dưới dạng dialog
         dialog.show();
     }
-    private void LoadDataFromServer(String studentID){
 
-        Map<String,Object> headers=new HashMap<>();
-        headers.put("token",token);
-        Call<ResponseObject> call=ApiUserRequester.getJsonPlaceHolderApi().getStudentInfo(headers,studentID);
+    // Trở về màn hình Dashboard của sinh viên
+    private void navigateToStudentDashboard() {
+        Intent intent = new Intent(StudentProfileActivity.this, StudentDashboardActivity.class);
+        intent.putExtra("studentID", studentID);
+        intent.putExtra("token", token);
+        intent.putExtra("userName", userName);
+        // Bắt đầu Activity tiếp theo với hiệu ứng chuyển động
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+    }
+
+    // Thay đổi mật khẩu
+    private void changePassword() {
+        if (!edtOld.getText().toString().isEmpty()) {
+            if (edtOld.getText().toString().equals(edtNew.getText().toString())) {
+                Toast.makeText(StudentProfileActivity.this, "The new password is duplicated than old password", Toast.LENGTH_LONG).show();
+            } else {
+                Map<String, Object> headers = new HashMap<>();
+                headers.put("token", token);
+                Map<String, Object> passInfo = new HashMap<>();
+                passInfo.put("userName", userName);
+                passInfo.put("oldHashPass", edtOld.getText().toString().trim());
+                passInfo.put("newHashPass", edtNew.getText().toString().trim());
+
+                Call<ResponseObject> call1 = ApiUserRequester.getJsonPlaceHolderApi().changePass(headers, passInfo);
+                call1.enqueue(new Callback<ResponseObject>() {
+                    @Override
+                    public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
+                        if (!response.isSuccessful()) {
+                            Toast.makeText(StudentProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                        ResponseObject tmp = response.body();
+                        if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
+                            Toast.makeText(StudentProfileActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
+                            return;
+                        }
+
+                        Toast.makeText(StudentProfileActivity.this, "Change Password successfully ", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseObject> call, Throwable t) {
+                        Toast.makeText(StudentProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }
+    }
+
+    // Thay đổi thông tin sinh viên
+    private void changeStudentInfo() {
+        if (!(edtName.getText().toString().equals(student.getName())
+                && (edtPhone.getText().toString().equals(student.getPhoneNumber())))) {
+            Map<String, Object> headers = new HashMap<>();
+            headers.put("token", token);
+            Map<String, Object> changeInfo = new HashMap<>();
+            changeInfo.put("userName", userName);
+            changeInfo.put("name", edtName.getText().toString());
+            changeInfo.put("phoneNumber", edtPhone.getText().toString());
+            changeInfo.put("facebook", edtFacebook.getText().toString());
+            changeInfo.put("roleCode", student.getRoleCode());
+
+            Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().changeProfile(headers, changeInfo);
+            call.enqueue(new Callback<ResponseObject>() {
+                @Override
+                public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
+                    if (!response.isSuccessful()) {
+                        Toast.makeText(StudentProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    ResponseObject tmp = response.body();
+                    if (tmp.getRespCode() != ResponseObject.RESPONSE_OK) {
+                        Toast.makeText(StudentProfileActivity.this, tmp.getMessage(), Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    Toast.makeText(StudentProfileActivity.this, "Change Data successfully ", Toast.LENGTH_LONG).show();
+
+                    navigateToStudentDashboard();
+                }
+
+                @Override
+                public void onFailure(Call<ResponseObject> call, Throwable t) {
+                    Toast.makeText(StudentProfileActivity.this, "Error", Toast.LENGTH_LONG).show();
+                }
+            });
+        } else {
+            Toast.makeText(StudentProfileActivity.this, "Nothing change information", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Load dữ liệu sinh viên từ server
+    private void loadDataFromServer(String studentID) {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("token", token);
+        Call<ResponseObject> call = ApiUserRequester.getJsonPlaceHolderApi().getStudentInfo(headers, studentID);
         call.enqueue(new Callback<ResponseObject>() {
             @Override
             public void onResponse(Call<ResponseObject> call, Response<ResponseObject> response) {
@@ -221,7 +242,7 @@ public class StudentProfileActivity extends AppCompatActivity {
                 }
 
                 Map<String, Object> data = (Map<String, Object>) tmp.getData();
-                student=new Student();
+                student = new Student();
                 student.setRoleCode(1);
                 student.setName(data.get("name").toString());
                 student.setPhoneNumber(data.get("phoneNumber").toString());
@@ -237,8 +258,9 @@ public class StudentProfileActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<ResponseObject> call, Throwable t) {
-                Toast.makeText(StudentProfileActivity.this,"Error load data",Toast.LENGTH_LONG).show();
+                Toast.makeText(StudentProfileActivity.this, "Error load data", Toast.LENGTH_LONG).show();
             }
         });
     }
 }
+
