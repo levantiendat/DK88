29f4d23261188b6d881a9e03df0b4d2a66a2e606
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-06-06 16:15:18 +0700

    Clean Code StudentDashboardActivity.java

--- app/src/main/java/com/example/dk88/StudentDashboardActivity.java
+++ app/src/main/java/com/example/dk88/StudentDashboardActivity.java
@@ -20,51 +20,85 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class StudentDashboardActivity extends AppCompatActivity {
-    CardView cvTrade, cvProfile, cvLogout;
+    private CardView cvTrade, cvProfile, cvLogout;
+    private String token;
+    private String studentID;
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_dashboard_layout);
-        String token=getIntent().getStringExtra("token");
-        String studentID=getIntent().getStringExtra("studentID");
-        String userName=getIntent().getStringExtra("userName");
-        cvTrade=(CardView) findViewById(R.id.tradeCourse);
-        cvProfile=(CardView) findViewById(R.id.userProfile);
-        cvLogout=(CardView) findViewById(R.id.logout);
 
+        // Nhận dữ liệu từ Intent
+        getDataFromIntent();
 
+        // Khởi tạo các view
+        initView();
+
+        // Xử lý sự kiện khi nhấn nút "Logout"
         cvLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(StudentDashboardActivity.this, SignInActivity.class);
-                startActivity(intent);
-                finish();
+                logout();
             }
         });
 
+        // Xử lý sự kiện khi nhấn nút "Profile"
         cvProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(StudentDashboardActivity.this, StudentProfileActivity.class);
-                intent.putExtra("token",token);
-                intent.putExtra("studentID",studentID);
-                intent.putExtra("userName",userName);
-                startActivity(intent);
-                finish();
+                goToStudentProfile();
             }
         });
 
+        // Xử lý sự kiện khi nhấn nút "Trade"
         cvTrade.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(StudentDashboardActivity.this, StudentAvailableGroupActivity.class);
-                intent.putExtra("token",token);
-                intent.putExtra("studentID",studentID);
-                intent.putExtra("userName",userName);
-                startActivity(intent);
-                finish();
+                goToAvailableGroups();
             }
         });
+    }
+
+    // Nhận dữ liệu từ Intent
+    private void getDataFromIntent() {
+        token = getIntent().getStringExtra("token");
+        studentID = getIntent().getStringExtra("studentID");
+        userName = getIntent().getStringExtra("userName");
+    }
+
+    // Khởi tạo các view
+    private void initView() {
+        cvTrade = findViewById(R.id.tradeCourse);
+        cvProfile = findViewById(R.id.userProfile);
+        cvLogout = findViewById(R.id.logout);
+    }
+
+    // Xử lý đăng xuất
+    private void logout() {
+        Intent intent = new Intent(StudentDashboardActivity.this, SignInActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    // Chuyển đến màn hình Student Profile
+    private void goToStudentProfile() {
+        Intent intent = new Intent(StudentDashboardActivity.this, StudentProfileActivity.class);
+        intent.putExtra("token", token);
+        intent.putExtra("studentID", studentID);
+        intent.putExtra("userName", userName);
+        startActivity(intent);
+        finish();
+    }
+
+    // Chuyển đến màn hình Available Groups
+    private void goToAvailableGroups() {
+        Intent intent = new Intent(StudentDashboardActivity.this, StudentAvailableGroupActivity.class);
+        intent.putExtra("token", token);
+        intent.putExtra("studentID", studentID);
+        intent.putExtra("userName", userName);
+        startActivity(intent);
+        finish();
     }
 }
