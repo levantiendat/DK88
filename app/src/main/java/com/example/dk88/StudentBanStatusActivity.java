e86f7eb0a2b5ac16f6398b189f532ab45d3396dd
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-06-06 22:21:47 +0700

    Clean StudentBanStatusActivity.java

--- app/src/main/java/com/example/dk88/StudentBanStatusActivity.java
+++ app/src/main/java/com/example/dk88/StudentBanStatusActivity.java
@@ -16,47 +16,72 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.dk88.View.SignInActivity;
 
 public class StudentBanStatusActivity extends AppCompatActivity {
-    Button btnOK;
-    TextView btnShow;
+    private Button btnOK;
+    private TextView btnShow;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_banned_status_layout);
-        btnOK=(Button) findViewById(R.id.ok);
-        btnShow=(TextView) findViewById(R.id.show);
+
+        initView();
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(StudentBanStatusActivity.this, SignInActivity.class);
-                startActivity(intent);
+                navigateToSignInActivity();
             }
         });
+
         btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Context context = StudentBanStatusActivity.this;
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View dialogView = inflater.inflate(R.layout.admin_information_dialog, null);
-
-                // Xây dựng AlertDialog
-                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                builder.setView(dialogView);
-                builder.setCancelable(true);
-
-                AlertDialog dialog = builder.create();
-                // Thiết lập kích thước cho dialog
-                WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
-                layoutParams.copyFrom(dialog.getWindow().getAttributes());
-                int width = (int) (context.getResources().getDisplayMetrics().widthPixels * 0.9);
-                int height = (int) (context.getResources().getDisplayMetrics().heightPixels * 0.9);
-                layoutParams.width = width;
-                layoutParams.height = height;
-                dialog.getWindow().setAttributes(layoutParams);
-
-                // Hiển thị dialog dưới dạng dialog
-                dialog.show();
+                showAdminInformationPopup();
             }
         });
+    }
+
+    private void initView() {
+        btnOK = findViewById(R.id.ok);
+        btnShow = findViewById(R.id.show);
+    }
+
+
+    private void navigateToSignInActivity() {
+        Intent intent = new Intent(StudentBanStatusActivity.this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+    private void showAdminInformationPopup() {
+        Context context = StudentBanStatusActivity.this;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View dialogView = inflater.inflate(R.layout.admin_information_dialog, null);
+
+        AlertDialog dialog = buildAlertDialog(context, dialogView);
+        setDialogSize(dialog, context);
+
+        // Display the dialog
+        dialog.show();
+    }
+
+    private AlertDialog buildAlertDialog(Context context, View dialogView) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setView(dialogView);
+        builder.setCancelable(true);
+
+        return builder.create();
+    }
+
+    private void setDialogSize(AlertDialog dialog, Context context) {
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.copyFrom(dialog.getWindow().getAttributes());
+
+        int width = (int) (context.getResources().getDisplayMetrics().widthPixels * 0.9);
+        int height = (int) (context.getResources().getDisplayMetrics().heightPixels * 0.9);
+
+        layoutParams.width = width;
+        layoutParams.height = height;
+
+        dialog.getWindow().setAttributes(layoutParams);
     }
 }
