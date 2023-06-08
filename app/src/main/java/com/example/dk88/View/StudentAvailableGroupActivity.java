6db6ac582dd71cba93f4805250e28f5576042bf5
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-06-08 09:56:03 +0700

    Delete Usage

--- app/src/main/java/com/example/dk88/View/StudentAvailableGroupActivity.java
+++ app/src/main/java/com/example/dk88/View/StudentAvailableGroupActivity.java
@@ -59,16 +59,12 @@ public class StudentAvailableGroupActivity extends AppCompatActivity {
 
     Map<String, String> needClass = new HashMap<>();
 
-    Graph g = new Graph();
-
     String studentID;
     String userName;
 
 
     int maxPage=0;
     int currentPage=1;
-
-    String oldGroup = null;
 
     private StudentAvailableGroupController mStudentAvailableGroupController;
 
