17c9af773e8f32faaccd3471e590fb9260537452
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-06-09 14:02:59 +0700

    Update Thread In StudentMyGroupInfoDialogActivity.java

--- app/src/main/java/com/example/dk88/View/StudentMyGroupInfoDialogActivity.java
+++ app/src/main/java/com/example/dk88/View/StudentMyGroupInfoDialogActivity.java
@@ -5,21 +5,26 @@ import androidx.annotation.NonNull;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
 import com.example.dk88.Controller.StudentMyGroupInfoDialogController;
+import com.example.dk88.Controller.StudentProfileController;
 import com.example.dk88.R;
 
 public class StudentMyGroupInfoDialogActivity extends Dialog implements View.OnClickListener {
-
     private Context context;
     private String token;
     private String studentID;
     private String groupID;
 
     private TextView tvStatus, tvWaitingList, tvJoinedList, tvStudent1, tvStudent2, tvStudent3, tvStudent4, tvStudent5;
+    private volatile boolean isRunning = true;
+    private Thread mainThread;
+    private Handler handler = new Handler(Looper.getMainLooper());
     private StudentMyGroupInfoDialogController mStudentMyGroupInfoDialogController;
 
     public StudentMyGroupInfoDialogActivity(@NonNull Context context, String token, String studentID, String groupID) {
@@ -62,4 +67,71 @@ public class StudentMyGroupInfoDialogActivity extends Dialog implements View.OnC
     public void onClick(View view) {
         // Handle click events
     }
+    private void clearDialogContents() {
+        tvStatus.setText("Status: ");
+        tvWaitingList.setText("Vote Yes: ");
+        tvJoinedList.setText("Waiting: ");
+        tvStudent1.setText("");
+        tvStudent2.setText("");
+        tvStudent3.setText("");
+        tvStudent4.setText("");
+        tvStudent5.setText("");
+
+    }
+
+    private class MainControlThread implements Runnable {
+        @Override
+        public void run() {
+            while (isRunning) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        clearDialogContents();
+
+                        mStudentMyGroupInfoDialogController = new StudentMyGroupInfoDialogController(context,token,studentID,groupID,tvStatus, tvWaitingList, tvJoinedList, tvStudent1,tvStudent2,tvStudent3,tvStudent4,tvStudent5);
+                        // Fetch student information and update UI
+                        mStudentMyGroupInfoDialogController.fetchStudentInfo();
+                        mStudentMyGroupInfoDialogController.retrieveGroupInformation();
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        startMainControlThread();
+    }
+
+    @Override
+    public void dismiss() {
+        super.dismiss();
+        stopMainControlThread();
+    }
+
+    private void startMainControlThread() {
+        isRunning = true;
+        mainThread = new Thread(new MainControlThread());
+        mainThread.start();
+    }
+
+    private void stopMainControlThread() {
+        isRunning = false;
+        if (mainThread != null) {
+            try {
+                mainThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
